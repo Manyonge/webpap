0cc38b8f49c64586de00c20f875948a8923cd222
Manyonge <77201345+Manyonge@users.noreply.github.com>
2023-06-27 17:55:05 +0300

    set up config provider theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,24 @@
-import './App.css'
-import {QueryClient, QueryClientProvider} from "react-query";
-import {RouterProvider} from "react-router-dom";
-import {router} from "./routes";
+import "./App.css";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./routes";
+import { ConfigProvider, ThemeConfig } from "antd";
+
+const theme: ThemeConfig = {
+  token: {
+    colorPrimary: "rgba(165, 126, 93, 1)",
+  },
+};
 
 function App() {
-const queryClient = new QueryClient()
+  const queryClient = new QueryClient();
   return (
     <QueryClientProvider client={queryClient}>
-<RouterProvider router={router}/>
+      <ConfigProvider theme={theme}>
+        <RouterProvider router={router} />
+      </ConfigProvider>
     </QueryClientProvider>
-  )
+  );
 }
 
-export default App
-
+export default App;
