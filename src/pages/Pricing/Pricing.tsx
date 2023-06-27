9e2ba2227895c486a30385c5c260e6751e09e394
Manyonge <77201345+Manyonge@users.noreply.github.com>
2023-06-27 20:19:31 +0300

    reached pricing filling content for first div we call it a day

--- src/pages/Pricing/Pricing.tsx
+++ src/pages/Pricing/Pricing.tsx
@@ -1,10 +1,25 @@
-import { WebpapNavBar } from "../../components";
+import { WebpapFooter, WebpapNavBar } from "../../components";
+import styles from "./style.module.css";
 
 export const Pricing = () => {
   return (
     <>
       <WebpapNavBar />
-      <p> Pricing page</p>
+      <div className={`${styles.msgOne} primaryBg`}>
+        <div></div>
+      </div>
+      <div className={styles.msgTwo}>
+        <ol className={styles.prosList}>
+          <li>
+            Custom e-commerce store front where your customers can explore and
+            shop
+          </li>
+          <li>Receive payments via M-PESA</li>
+          <li>Deliver country wide with Pickup Mtaani</li>
+          <li>Free advertising on our market place</li>
+        </ol>
+      </div>
+      <WebpapFooter />
     </>
   );
 };
