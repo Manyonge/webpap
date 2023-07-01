14e39a621bdaa0373130b70e3de68d305ac02815
Manyonge <77201345+Manyonge@users.noreply.github.com>
2023-07-01 16:24:35 +0300

    single order page basics laid out

--- src/pages/Admin/SingleOrder/SingleOrder.tsx
+++ src/pages/Admin/SingleOrder/SingleOrder.tsx
@@ -1,3 +1,44 @@
+import { useState } from "react";
+import { Order } from "../../../common";
+import { LeftOutlined } from "@ant-design/icons";
+import { Link, useParams } from "react-router-dom";
+
 export const SingleOrder = () => {
-  return <p>Single order page</p>;
+  const [order, setOrder] = useState<Order>({
+    productPhoto:
+      "https://hustle.imgix.net/8wb0abtw2etmk90e6l6vidnjefe1w88d.jpeg?fit=crop&w=512&h=512",
+    productName: "Jordan 1 lows",
+    amountPaid: 3500,
+    orderTime: "2022-7-1 10:31AM",
+    customerName: "Arthur Manyonge",
+    orderId: "iidufieuf",
+    isFulFilled: false,
+  });
+
+  const { storeFrontID } = useParams();
+
+  return (
+    <div className="px-10 md:px-44 pt-10 ">
+      <Link to={`/${storeFrontID}/admin/orders`}>
+        <button className="flex flex-row items-center justify-center mb-4">
+          {" "}
+          <LeftOutlined />
+          Back{" "}
+        </button>
+      </Link>
+      <div className=" py-4 rounded-lg shadow-lg text-center ">
+        <img
+          src={order.productPhoto}
+          className="rounded-lg h-44 w-44 mx-auto "
+        />
+        <p> {order.productName} </p>
+        <p> {order.amountPaid} </p>
+        <p> Ordered at {order.orderTime} </p>
+      </div>
+
+      <button className="  shadow-lg rounded-full mt-10 w-full text-[#fff] bg-[#416C85]">
+        Fulfill
+      </button>
+    </div>
+  );
 };
