5fb51595151c4308e6d9f7856cfe41b4e599a44e
Manyonge <77201345+Manyonge@users.noreply.github.com>
2023-07-01 12:26:02 +0300

    orders page basics done

---
+++ src/common/interfaces.ts
@@ -0,0 +1,9 @@
+export interface Order {
+  amountPaid: number;
+  customerName: string;
+  orderTime: string;
+  productName: string;
+  productPhoto: string;
+  orderId: string;
+  isFulFilled: boolean;
+}
