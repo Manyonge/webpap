5cd42aac1fb9a97d6bb97fd77537b68d7d1b1648
Manyonge <77201345+Manyonge@users.noreply.github.com>
2023-07-01 09:27:57 +0300

    uninstalled antd

--- src/pages/Admin/Orders/Orders.tsx
+++ src/pages/Admin/Orders/Orders.tsx
@@ -1,3 +1,20 @@
+import * as Tabs from "@radix-ui/react-tabs";
+
 export const Orders = () => {
-  return <p>All Orders page </p>;
+  return (
+    <Tabs.Root className="TabsRoot" defaultValue="tab1">
+      <Tabs.List className="TabsList">
+        <Tabs.Trigger className="TabsTrigger" value="tab1">
+          All
+        </Tabs.Trigger>
+        <Tabs.Trigger className="TabsTrigger" value="tab2">
+          New
+        </Tabs.Trigger>
+        ```
+        <Tabs.Trigger className="TabsTrigger" value="tab3">
+          Fulfilled
+        </Tabs.Trigger>
+      </Tabs.List>
+    </Tabs.Root>
+  );
 };
