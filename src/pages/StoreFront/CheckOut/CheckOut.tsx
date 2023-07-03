1bb5f0a372235105163edf78ce91d19af042ee5f
Manyonge <77201345+Manyonge@users.noreply.github.com>
2023-07-03 20:11:33 +0300

    in the process of finishing laying out basics for checkout page

--- src/pages/StoreFront/CheckOut/CheckOut.tsx
+++ src/pages/StoreFront/CheckOut/CheckOut.tsx
@@ -1,3 +1,18 @@
+import { Link, useParams } from "react-router-dom";
+import { LeftOutlined } from "@ant-design/icons";
+
 export const CheckOut = () => {
-  return <></>;
+  const { storeFrontID } = useParams();
+  return (
+    <div className="px-10 pb-40">
+      <Link to={`/${storeFrontID}`} className="font-bold mb-4 ">
+        <LeftOutlined />
+        Cancel
+      </Link>
+
+      <form>
+        <input />
+      </form>
+    </div>
+  );
 };
