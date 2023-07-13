1864d0c25ea74c44a2a9bc8a100aa506bf0cf48c
Arthur Manyonge <77201345+Manyonge@users.noreply.github.com>
2023-07-13 21:09:43 +0300

    removed supabaseApi idea completely

--- src/components/RequireAuth.tsx
+++ src/components/RequireAuth.tsx
@@ -1,18 +1,14 @@
 import { useEffect, useState } from "react";
-import { useAppContext } from "../contexts/AppContext.tsx";
 import { supabase } from "../supabase.ts";
 import { Navigate } from "react-router-dom";
 
 export const RequireAuth = (props: { children: any }) => {
   const { children } = props;
   const [isSessionExpired, setIsSessionExpired] = useState(false);
-  const { supabaseApi } = useAppContext();
 
   useEffect(() => {
     const authenticate = async () => {
-      const { data: sessionData } = await supabaseApi(
-        supabase.auth.getSession(),
-      );
+      const { data: sessionData } = await supabase.auth.getSession();
       if (!sessionData.session) setIsSessionExpired(true);
     };
     authenticate().then();
