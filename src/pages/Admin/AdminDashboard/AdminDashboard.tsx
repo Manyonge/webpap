ce8f8dad7e56e6569fc7bc8c6c7d5c091e3c7569
Manyonge <77201345+Manyonge@users.noreply.github.com>
2023-07-19 19:50:16 +0300

    admin dashboard styling and functionality complete

--- src/pages/Admin/AdminDashboard/AdminDashboard.tsx
+++ src/pages/Admin/AdminDashboard/AdminDashboard.tsx
@@ -62,9 +62,7 @@ export const AdminDashboard = () => {
     <>
       <img
         src={retailer?.businessLogo as string}
-        alt="your business logo"
         className="mx-auto my-7 w-28 h-28 md:w-32 md:h-32 rounded-full
-        text-center
         "
       />
 
@@ -75,20 +73,27 @@ export const AdminDashboard = () => {
       <div className="px-4 mx-auto md:w-10/12 grid grid-cols-2 gap-6 md:gap-10  ">
         {currentStats.map(({ label, value, info, route }, index) => (
           <Link to={route} key={index}>
-            <div className="rounded-lg shadow-lg hover:shadow-xl h-40 ">
-              <div className="bg-primary text-[#fff] h-3/5 md:h-3/4 rounded-tr-lg rounded-tl-lg py-4 ">
-                <p className="text-[#fff] text-center text-3xl md:text-6xl font-bold ">
-                  {" "}
-                  {value}{" "}
-                </p>
+            <div className="rounded-lg shadow-lg hover:shadow-xl h-40">
+              <div className="bg-primary text-[#fff] h-3/5 md:h-3/4 rounded-tr-lg rounded-tl-lg pt-4 ">
+                {statsQuery.isLoading && (
+                  <p className="text-[#fff] text-center text-3xl md:text-6xl font-bold ">
+                    ...
+                  </p>
+                )}
+                {statsQuery.isSuccess && (
+                  <p className="text-[#fff] text-center text-3xl md:text-6xl font-bold ">
+                    {" "}
+                    {value}{" "}
+                  </p>
+                )}
                 <p className="text-[#fff] text-center text-lg md:text-xl font-bold ">
                   {" "}
                   {label}{" "}
                 </p>
               </div>
-              <div className="h-1/4">
+              <div className=" flex flex-col items-center justify-center pt-1 ">
                 {" "}
-                <p className="text-center text-sm px-2 py-1"> {info} </p>{" "}
+                <p className="text-center text-sm px-2 "> {info} </p>{" "}
               </div>
             </div>
           </Link>
