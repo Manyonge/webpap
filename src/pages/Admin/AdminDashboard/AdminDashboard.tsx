772e31e9460ccdf88e8c2b54c3b6b122d8134f04
Manyonge <77201345+Manyonge@users.noreply.github.com>
2023-07-19 19:21:35 +0300

    admin dashboard page connected server

--- src/pages/Admin/AdminDashboard/AdminDashboard.tsx
+++ src/pages/Admin/AdminDashboard/AdminDashboard.tsx
@@ -1,37 +1,58 @@
-import { useState } from "react";
 import { Link } from "react-router-dom";
 import { useGetRetailer } from "../../../common/hooks";
+import { supabase } from "../../../supabase.ts";
+import { useQuery } from "react-query";
 
 export const AdminDashboard = () => {
   const { retailer } = useGetRetailer();
 
-  const [currentRevenue, setCurrentRevenue] = useState(`${100} KSH`);
-  const [currentProducts, setCurrentProducts] = useState(100);
-  const [currentOrders, setCurrentOrders] = useState(100);
-  const [currentCustomers, setCurrentCustomers] = useState(100);
+  const fetchStats = async () => {
+    const stats = { revenue: 0, products: 0, orders: 0, customers: 0 };
+    const { data: sessionData } = await supabase.auth.getSession();
+
+    const { data: revenue } = await supabase
+      .from("retailers")
+      .select("walletBalance")
+      .eq("id", sessionData.session?.user.id)
+      .single();
+    revenue ? (stats.revenue = revenue.walletBalance) : null;
+
+    const { data: totalProducts } = await supabase.from("products").select();
+    totalProducts ? (stats.products = totalProducts.length) : null;
+
+    const { data: orders } = await supabase.from("orders").select();
+    orders ? (stats.orders = orders.length) : null;
+
+    const { data: customers } = await supabase.from("customers").select();
+    customers ? (stats.customers = customers.length) : null;
+
+    return stats;
+  };
+
+  const statsQuery = useQuery("stats", fetchStats);
 
   const currentStats = [
     {
       label: "Revenue",
-      value: currentRevenue,
+      value: statsQuery.data?.revenue.toLocaleString(),
       info: "Sales you've made so far",
       route: "wallet",
     },
     {
       label: "Products",
-      value: currentProducts,
+      value: statsQuery.data?.products,
       info: "Manage your products",
       route: "products",
     },
     {
       label: "Orders",
-      value: currentOrders,
+      value: statsQuery.data?.orders,
       info: "Manage your orders",
       route: "orders",
     },
     {
       label: "Customers",
-      value: currentCustomers,
+      value: statsQuery.data?.customers,
       info: " customers you've served so far",
       route: "customers",
     },
