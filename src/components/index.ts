a17032dee5363ea0720f386b740140de86eb9274
manyonge <arthurmanyonge22@gmail.com>
2023-11-08 23:57:36 +0300

    - extracted product search component - implemented product search component in uploaded products page and store front layout

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,3 +3,4 @@ export { WebpapFooter } from "./WebpapFooter.tsx";
 export { ImageInput } from "./ImageInput.tsx";
 export { RequireAuth } from "./RequireAuth.tsx";
 export { LoadingIndicator } from "./LoadingIndicator.tsx";
+export { ProductSearch } from "./ProductSearch.tsx";
