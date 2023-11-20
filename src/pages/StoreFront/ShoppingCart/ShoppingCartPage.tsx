a0101f7405c441cc3231100a0469b9e0e1e5d827
manyonge <arthurmanyonge22@gmail.com>
2023-11-20 19:48:40 +0300

    - shopping cart page looking and working good for now

--- src/pages/StoreFront/ShoppingCart/ShoppingCartPage.tsx
+++ src/pages/StoreFront/ShoppingCart/ShoppingCartPage.tsx
@@ -24,19 +24,19 @@ const CartItem = (props: {
 
   return (
     <div
-      className="rounded-lg shadow-xl px-3 py-2 mb-4
+      className="rounded-lg shadow-xl px-3 py-2 mb-6
    flex flex-col items-center   justify-between
-   mb-6"
+   "
     >
       <div className=" flex flex-row items-center justify-between  w-full">
         <img
-          src={product.product_images[0].url}
+          src={product?.product_images[0]?.url}
           className="
         object-fill rounded-lg w-14 h-14  "
         />
         <p className="text-center"> {product.name} </p>
         <p className="text-center"> {product.size} </p>
-        <p className="text-center"> {product.price} </p>
+        <p className="text-center"> {`KSH ${product.price}`} </p>
       </div>
       <div
         className="flex flex-col items-center justify-between
@@ -77,32 +77,34 @@ export const ShoppingCartPage = () => {
       <Link
         to={`/${storeFrontId}`}
         className="flex flex-row items-center justify-start
-       font-bold mb-4
+        mb-4 w-11/12 mx-auto
       "
       >
         <LeftOutlined />
         <p>Continue Shopping</p>
       </Link>
 
-      <p className="text-lg text-center font-bold mb-5 "> Shopping Cart </p>
+      <p className="text-xl text-center  mb-5 "> Shopping Cart </p>
 
       <div className="flex flex-row items-center justify-between mb-5 w-11/12   mx-auto">
-        <p className="font-bold">TOTAL</p>
-        <p className="font-bold"> {shoppingCart.totalPrice} KSH </p>
+        <p>TOTAL</p>
+        <p> {shoppingCart.totalPrice} KSH </p>
       </div>
 
       <Link
         to={`/${storeFrontId}/checkout`}
         className="flex flex-row justify-center items-center"
       >
-        <button className="w-11/12 mx-auto  rounded-full shadow-lg mb-12 bg-success text-white py-1 ">
-          {" "}
-          Check out{" "}
-        </button>
+        {shoppingCart.products.length > 0 && (
+          <button className="w-11/12 mx-auto  rounded-full shadow-lg mb-12 bg-success text-white py-1 ">
+            {" "}
+            Check out{" "}
+          </button>
+        )}
       </Link>
 
       {shoppingCart.products.length < 1 && (
-        <p className="text-center text-lg"> No items added </p>
+        <p className="text-center text-error text-lg"> No items added </p>
       )}
 
       <div className="w-11/12 mx-auto">
