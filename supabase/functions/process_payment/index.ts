5d3d8e0ff7e4c9918bb9778f6107706c851dc27c
manyonge <arthurmanyonge22@gmail.com>
2023-11-24 10:47:58 +0300

    - implemented process payment function basics successfully

--- supabase/functions/process_payment/index.ts
+++ supabase/functions/process_payment/index.ts
@@ -1,10 +1,23 @@
-Deno.serve(async (req) => {
-  const { name } = await req.json();
-  const data = {
-    message: `Hello ${name}, we received your request!`,
-  };
+import { CheckOut } from "../../../src/common/interfaces/index.ts";
+import { corsHeaders } from "../../_shared/cors.ts";
 
-  return new Response(JSON.stringify(data), {
-    headers: { "Content-Type": "application/json" },
-  });
+Deno.serve(async (req) => {
+  if (req.method === "OPTIONS") {
+    return new Response("ok", { headers: corsHeaders });
+  }
+  try {
+    const data: CheckOut = await req.json();
+    const response = {
+      message: `${data.name}'s order has been received`,
+    };
+
+    return new Response(JSON.stringify(response), {
+      headers: { ...corsHeaders, "Content-Type": "application/json" },
+    });
+  } catch (error) {
+    return new Response(JSON.stringify({ error: error.message }), {
+      headers: { ...corsHeaders, "Content-Type": "application/json" },
+      status: 400,
+    });
+  }
 });
